ebabde54b507c7248b2b8a502983a14fb32d4df0
Eggcanoe <tommyhalleland@hotmail.com>
2024-02-15 10:26:32 +0100

    added comments

--- stateObject/src/App.jsx
+++ stateObject/src/App.jsx
@@ -5,11 +5,22 @@ import starUnFav from "./assets/starUnFav.jpg";
 import "./AppStyling.css";
 
 function App() {
+  //Contains various information about person
+  const [contact, setContact] = React.useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "445-555-202",
+    email: "myemail@adress.com",
+    isFavorite: false,
+  });
+
   return (
     <div>
       <main>
+        {/*Profile picture*/}
+        <img src={ProfileImage}></img>
         <article>
-          <img src={ProfileImage}></img>
+          {/*Displays various information for the person*/}
           <h2>John Doe</h2>
           <img className="favorite" src={starUnFav} />
           <p>445-555-202</p>
