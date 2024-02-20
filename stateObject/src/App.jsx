894954566da630c5923bc1ba335ea5626f3b5346
Tommyhh04 <tommyhh04.kodehode@gmail.com>
2024-02-20 09:44:10 +0100

    Added comments explaining the code more in depth

--- stateObject/src/App.jsx
+++ stateObject/src/App.jsx
@@ -5,7 +5,7 @@ import starUnFav from "./assets/starUnFav.jpg";
 import "./AppStyling.css";
 
 function App() {
-  //Contains various information about person
+  //Contains various information about person and stores it in a object
   const [contact, setContact] = React.useState({
     firstName: "John",
     lastName: "Doe",
@@ -14,8 +14,9 @@ function App() {
     isFavorite: true,
   });
 
+  //This ternary switches between favorite star or unfavorite star depending on the true or false state of isFavorite in contact object
   let starIcon = contact.isFavorite ? starFav : starUnFav;
-
+  //This function toggles between the isFavorite states making it change to true or false using the "!" to make it change to the opposite of what the state already is
   function toggleFavorite() {
     setContact((prevContact) => ({
       ...prevContact,
@@ -31,6 +32,7 @@ function App() {
         <article>
           {/*Displays various information for the person*/}
           <h2>John Doe</h2>
+          {/*Favorite button*/}
           <img className="favorite" src={starIcon} onClick={toggleFavorite} />
           <p>445-555-202</p>
           <p>myemail@adress.com</p>
